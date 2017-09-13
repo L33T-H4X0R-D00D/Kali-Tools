98016c213d22a87fa06de75c1e26ec6fc06d6190
L33T-H4X0R-D00D <adambrookman@gmail.com>
2017-09-13 01:57:53 -0400

    Significant config update
    Updated and tested most of the configuration script.

--- kalisetup.sh
+++ kalisetup.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
 #Version 20170912.1
-#This script assumes you're logged in as root using the Kali VM provided by Offensive Security here: https://www.offensive-security.com/kali-linux-vmware-virtualbox-image-download/.
+#This script assumes you're logged in as root using the Kali 2017.2 VM provided by Offensive Security here: https://www.offensive-security.com/kali-linux-vmware-virtualbox-image-download/.
 #A browser will be opened to the Java, and Nessus download page allowing you to pick which version to install.
 #After the download is complete the script will complete the install from the downloads directory.
 #This script is setup to make it easy to comment out the components you do not wish to install, not be the most efficient use of space.
 #This script creates a directory on the desktop with scripts that update tools, scripts that run tools, and some basic instructions for tools.
 
 
+#Create directory structure.
 #Install Java without modifying sources.
 #Roll SSH keys.
 #Update the system.
@@ -30,7 +32,8 @@
 #Create script on desktop to update Exploit Pack.
 #Create script on desktop to update Veil Evasion 3.
 
-#!/bin/bash
+#Create directory structure.
+mkdir /root/Desktop/Scripts && mkdir /root/Desktop/Scripts/instructions && mkdir /root/Desktop/Scripts/tools && mkdir /root/Desktop/Scripts/update
 
 #Java download.
 firefox http://www.java.com/en/download/linux_manual.jsp
@@ -44,15 +47,16 @@ firefox https://github.com/gophish/gophish/releases
 #Install Java without modifying sources.
 cd /root/Downloads/
 tar -zxvf jre*.tar.gz
+rm jre*.tar.gz
 mv jre* /opt
 cd /opt/jre*
-update-alternatives –install /usr/bin/java java /opt/jre*/bin/java 1
-update-alternatives –install /usr/lib/mozilla/plugins/libjavaplugin.s­o mozilla-javaplugin.so /opt/jre*/lib/amd64/libnpjp2.so 1
-update-alternatives –set java /opt/jre*/bin/java
-update-alternatives –set javac /opt/jre*/bin/javac
-update-alternatives –set mozilla-javaplugin.so /opt/jre*/lib/amd64/libnpjp2.so
+update-alternatives --install /usr/bin/java java /opt/jre*/bin/java 1
+update-alternatives --install /usr/lib/mozilla/plugins/libjavaplugin.s­o mozilla-javaplugin.so /opt/jre*/lib/amd64/libnpjp2.so 1
+update-alternatives --set java /opt/jre*/bin/java
+update-alternatives --set javac /opt/jre*/bin/javac
+update-alternatives --set mozilla-javaplugin.so /opt/jre*/lib/amd64/libnpjp2.so
 java -version
-pause
+sleep 10s
 
 #Roll SSH keys
 cd /etc/ssh/ && mkdir old #Move to the SSH directory and make a new directory for the old keys.
@@ -61,54 +65,90 @@ dpkg-reconfigure openssh-server #Create new SSH keys.
 
 #Update system and install tools.
 apt update && apt upgrade -y && apt dist-upgrade -y
-apt install tor
-apt install filezilla filezilla-common
-apt install alien rpm nsis openvas
-apt install htop nethogs gdebi git bleachbit clamav #System utilities.
-freshclam #Update ClamAV footprints.
-apt install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential #For pwntools.
-
-
-#Create new openvas user of "admin" with a password of "toor".
+apt install tor -y
+apt install filezilla filezilla-common -y
+apt install alien rpm nsis openvas -y
+apt install htop nethogs gdebi git bleachbit clamav -y #System utilities.
+apt install python2.7 python-pip python-dev git libssl-dev libffi-dev build-essential -y #For pwntools.
 
 
 
+#Perform initial OpenVAS setup.
+openvas-mkcert
+openvas-mkcert-client -n -i
 #Update OpenVAS NVT, CERT, and SCAP data.
 greenbone-nvt-sync
 greenbone-certdata-sync
 greenbone-scapdata-sync
+#Start OpenVAS services and create DB.
+systemctl start openvas-scanner
+openvasmd --rebuild --progress
+openvasmd --create-user=admin --role=Admin --new-password=toor #Create new openvas user of "admin" with a password of "toor".
+openvassd
+openvasmd
+gsad
+openvas-check-setup
+sleep 10s
 
+#Create OpenVAS startup script
+mkdir /root/Desktop/Scripts/tools/openvas/
+echo '#!/bin/bash' >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo openvassd >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo openvasmd >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo gsad >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo greenbone-nvt-sync >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo greenbone-certdata-sync >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo greenbone-scapdata-sync >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo openvas-check-setup >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo sleep 10s >> /root/Desktop/Scripts/tools/openvas/start.sh
+echo firefox https://127.0.0.1/l >> /root/Desktop/Scripts/tools/openvas/start.sh
+chmod +x /root/Desktop/Scripts/tools/openvas/start.sh
 
 #Install Nessus
-gdebi -i Nessus*.deb
+gdebi -n /root/Downloads/Nessus*.deb
 
+#Create Nessus startup script
+mkdir /root/Desktop/Scripts/tools/nessus/
+echo '#!/bin/bash' >> /root/Desktop/Scripts/tools/nessus/start.sh
+echo /etc/init.d/nessusd start  >> /root/Desktop/Scripts/tools/nessus/start.sh
+echo firefox https://127.0.0.1:8834/  >> /root/Desktop/Scripts/tools/nessus/start.sh
+chmod +x /root/Desktop/Scripts/tools/nessus/start.sh
 
 #Fix any broken installs.
-apt install -f
+apt install -f && apt autoremove && apt autoclean #Cleanup
 
 
-#Install Exploit Pack
+#Install Exploit Pack.
 git clone https://github.com/juansacco/exploitpack.git /usr/share/exploitpack
-cd /usr/share/exploitpack
-java -jar ExploitPack.jar
+
+#Create Exploit Pack startup script.
+mkdir /root/Desktop/Scripts/tools/exploitpack/
+echo '#!/bin/bash' >> /root/Desktop/Scripts/tools/exploitpack/start.sh
+echo cd /usr/share/exploitpack >> /root/Desktop/Scripts/tools/exploitpack/start.sh
+echo java -jar ExploitPack.jar >> /root/Desktop/Scripts/tools/exploitpack/start.sh
+chmod +x /root/Desktop/Scripts/tools/exploitpack/start.sh
 
 
-
-#Install Veil Evasion 3
-git clone --recursive https://github.com/Veil-Framework/Veil.git /usr/share/Veil
+#Install Veil Evasion 3.
+git clone https://github.com/Veil-Framework/Veil.git /usr/share/Veil
 cd /usr/share/Veil/setup
+#./setup.sh -s
 ./setup.sh -c
 
 
-
 #Install GoPhish
-cd ~/Downloads
-unzip gophish-v???-linux-64bit.zip -d /usr/share/
-cd /usr/share/gophish-v???-linux-64bit
+cd /root/Downloads
+unzip 'gophish-*.zip' -d /usr/share/
+cd /usr/share/gophis*
 chmod 755 ./gophish
-./gophish & firefox https://localhost:3333 &
 
 
+
+#Create GoPhish startup script and credentials document.
+mkdir /root/Desktop/Scripts/tools/gophish/
+echo './gophish & firefox https://localhost:3333 &' >> /root/Desktop/Scripts/tools/gophish/start.sh
+echo Username: admin  >> /root/Desktop/Scripts/tools/gophish/login.txt
+echo Password: gophish >> /root/Desktop/Scripts/tools/gophish/login.txt
 
 
 #Install pwntools
@@ -140,83 +180,6 @@ updatedb
 xset s off # don't activate screensaver
 xset -dpms # disable DPMS (Energy Star) features.
 xset s noblank # don't blank the video device
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Download instructions for use from GIT.
-
-wget -O new_setup.sh https://gist.githubusercontent.com/BeanBagKing/198683e3206eb8885a1a20f6adb6e7e6/raw  
-chmod 755 new_setup.sh  
-./new_setup.sh
-
-wget --quiet -O /usr/local/bin/decode.sh https://gist.githubusercontent.com/BeanBagKing/019252de635f8748d36e47ee2d57d3b1/raw
-	chmod 755 /usr/local/bin/decode.sh
-
-
-
-#Download 
-
-
-
-
-#Create scripts to start TOR
-service tor start
-proxychains iceweasel
-
-
-
-#Update Veil Evasion 3
-cd /usr/share/Veil && git pull
-
-
-
-
-#Update Exploit Pack
-cd /usr/share/exploitpack && git pull
-
-
-
-
-
-#Run Lynis
-lynis audit system --quick
-
-
-
-#Scan for printers with PRET https://github.com/RUB-NDS/PRET
-echo Identify the printer you wish to attack and run "pret.py <IP Address> <printer language>". Available printer languages are PS (Postscript), PJL (Printer Job Language), and PCL (Printer Command Language).
-./pret.py
-
-
-
-
-#Start Nessus.
-/etc/init.d/nessusd start
-sleep 10s
-firefox https://127.0.0.1:8834/
-
-
-
-
-
-
-
-
 
 
 
